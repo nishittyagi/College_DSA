b0637aa34b0eaae97e98ce834692a2fc8515f353
Nishit tyagi <114007266+nishittyagi@users.noreply.github.com>
2022-11-01 11:51:05 +0530

    Create string.cpp

---
+++ ASSIGNMENT 1/string.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string a,b;
+    cin>>a>>b;
+    cout<<a.size()<<" "<<b.size()<<endl;
+    string c=a+b;
+    cout<<c<<endl;
+    c=a;
+    a[0]=b[0];
+    b[0]=c[0];
+    cout<<a<<" "<<b;
+    return 0;
+}
