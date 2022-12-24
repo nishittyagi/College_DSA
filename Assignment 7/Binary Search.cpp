219d4eb5480285e34e55aee35e2fac63fa0ecefa
Nishit tyagi <114007266+nishittyagi@users.noreply.github.com>
2022-12-24 22:56:13 +0530

    Create Binary Search.cpp

---
+++ Assignment 7/Binary Search.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int size=nums.size(),start=0,end=size-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(nums[mid]<target) start=mid+1;
+            else if(nums[mid]>target) end=mid-1;
+            else if(nums[mid]==target) return mid;
+            else return end;
+        }
+        return -1;
+    }
+};
